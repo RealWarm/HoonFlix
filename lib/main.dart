3bc4dd615590dfacd9bdac41ef5714f39eecded3
RealWarm <wjdgns8946@naver.com>
2023-10-09 17:38:38 +0900

    첫번째 커밋

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hello Hoon!!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ////////////////////////////////////////////
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total balance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              /////////////////////////////////////////////////////////
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button(
+                    text: "Transfer",
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: "request",
+                    bgColor: Color(0xFF1f2123),
+                    textColor: Colors.white,
+                  ),
+                ], //children
+              ),
+              /////////////////////////////////////////////////////////
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// class MyButton extends StatelessWidget {
+//   const MyButton({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(45),
+//         color: const Color(0xFF1f2123),
+//       ),
+//       child: const Padding(
+//         padding: EdgeInsets.symmetric(
+//           vertical: 20,
+//           horizontal: 50,
+//         ),
+//         child: Text(
+//           'request',
+//           style: TextStyle(
+//             color: Colors.white,
+//             fontSize: 20,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// } //end class
+
+// build : 플러터가 실행하는 메소드로widget의 ui를 만듬
+// Material : 구글 || Cupertino : ios
